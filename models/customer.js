fa40471cc175e26a2c124abe0b0cfd956de9eb58
daroo22 <user@email.com>
2024-07-31 15:51:03 -0400

    finished lab

---
+++ models/customer.js
@@ -0,0 +1,10 @@
+const customerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+ 
+    age: { type: Number, required: true },
+  
+  });
+
+const Customer = mongoose.Model("Customer", customerSchema);
+
+export default `Customer`;
